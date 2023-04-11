2b7ddc914018201a8d019050390a7c37f98aa774
GitHub-El-Pinillo <123605635+GitHub-El-Pinillo@users.noreply.github.com>
2023-04-11 11:08:32 +0100

    Create displayInfo.py
    código para desplegar información inicial

---
+++ displayInfo.py
@@ -0,0 +1,7 @@
+# CREACION DE UN ARCHIVO python
+
+# - Es posible crear archivos de código, como por ejemplo html, css, js, py, etc
+
+# Despliega información
+
+print("Este es un repositorio de pruebas en python")
